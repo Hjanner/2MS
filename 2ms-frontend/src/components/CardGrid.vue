ea8b551606707001f5c9b1ad8524f120d834635f
Carlos <cca.gomez2014@gmail.com>
2025-08-02 02:26:24 -0400

    Add Cart.vue file to components folder

---
+++ 2ms-frontend/src/components/CardGrid.vue
@@ -0,0 +1,20 @@
+<script setup>
+  const { items } = defineProps({
+    items: Array,
+  })
+</script>
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="(item, key) in items"
+        :key
+        cols="12"
+        lg="3"
+      >
+        <Card :item />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ 2ms-frontend/src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<script setup>
+  //
+</script>
+
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    rail
+  >
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-alpha-m-box"
+        subtitle="Sistema de gestión"
+        title="MS2"
+      />
+    </v-list>
+
+    <v-divider />
+
+    <v-list nav>
+      <v-list-item
+        link
+        prepend-icon="mdi-home"
+        title="Inicio"
+        value="inicio"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
